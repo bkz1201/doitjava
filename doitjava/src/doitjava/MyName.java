dd7e368f08edf7866091717213829f5561d7795f
seokjin <Yseokjin.dev@gmail.com>
2020-08-11 14:55:34 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/MyName.java
@@ -0,0 +1,32 @@
+package doitjava;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		System.out.println("제 이름은 윤석진입니다.");
+		System.out.println("직업은 백수입니다");
+		/**
+		 * (asterisk)를 2개 사용한 주석은
+		 * 도큐먼터리 주석으로 문서의 정보를
+		 * 나타낼 때 사용합니다.
+		 * 작성자 윤아무개.
+		 * 작성일 8월 셋째주 월요일 피곤...
+		 */
+		/* 가
+		 * 나
+		 * 다
+		 * 라
+		 * 마
+		 * 바
+		 * 사
+		 * 아
+		 * 자
+		 * 차
+		 * 카
+		 * 타
+		 * 파
+		 * 하*/
+	}
+
+}
