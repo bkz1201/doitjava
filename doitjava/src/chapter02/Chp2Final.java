4c9b3196191f4804ff80d9d219eed806de9b2a8e
seokjin <Yseokjin.dev@gmail.com>
2020-08-14 14:56:36 +0900

    챕터2 연습문제

---
+++ doitjava/src/chapter02/Chp2Final.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class Chp2Final {
+	public static void main(String[] args) {
+		int iNum = 10;
+		double dNum = 2.0;
+		int iRes = iNum + (int)dNum;
+		
+		System.out.println(iRes);	//연습문제 4
+		
+		int iVal = '글';
+		char cVal = (char)iVal;
+		
+		System.out.println(cVal);
+	}
+}
