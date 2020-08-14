23d575d2faf34c640628a3f7ab437c7b25164796
seokjin <Yseokjin.dev@gmail.com>
2020-08-14 13:43:18 +0900

    챕터2 - boolean 논리자료형에 대한 연습

---
+++ doitjava/src/chapter02/BolleanEx.java
@@ -0,0 +1,8 @@
+package chapter02;
+
+public class BolleanEx {
+	public static void main(String[] args) {
+		boolean isMarried = true;	//boolean 변수 형은 'true' 혹은 false만 사용가능하다.
+		System.out.println(isMarried);
+	}
+}
