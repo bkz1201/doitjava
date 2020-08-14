398c61d5edfaa817ad2333602487816f84dc39b0
seokjin <Yseokjin.dev@gmail.com>
2020-08-14 15:32:30 +0900

    챕터3 연산자 예제

---
+++ doitjava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+public class ArithmeticOperator {
+	public static void main(String[] args) {
+		int iRes;
+		double dRes;
+		
+		iRes = 3+2;
+		System.out.println(iRes);
+		iRes= 3-2;
+		System.out.println(iRes);
+		iRes=3*2;
+		System.out.println(iRes);
+		iRes=3/2;
+		System.out.println(iRes);
+		
+		dRes=3.0/2.0;
+		System.out.println(dRes);
+		iRes=3%2;
+		System.out.println(iRes);
+	}
+}
