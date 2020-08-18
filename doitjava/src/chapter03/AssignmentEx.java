7c98435a9547e1ae3520e6e4982140d3d8cd9268
seokjin <Yseokjin.dev@gmail.com>
2020-08-18 14:53:47 +0900

    챕터3 복합대입연산자

---
+++ doitjava/src/chapter03/AssignmentEx.java
@@ -0,0 +1,17 @@
+package chapter03;
+
+public class AssignmentEx {
+	public static void main(String[] args) {
+		int res = 0;
+		res += 10;
+		System.out.println("res = " + res);
+		res -= 5;
+		System.out.println("res = " + res);
+		res *= 3;
+		System.out.println("res = " + res);
+		res /= 5;
+		System.out.println("res = " + res);
+		res %= 3;
+		System.out.println("res = " + res);
+	}
+}
