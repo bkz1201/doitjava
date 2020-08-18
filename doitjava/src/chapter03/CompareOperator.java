f0c42a22495ae435e7f7b76fd5ad76698065f3a0
seokjin <Yseokjin.dev@gmail.com>
2020-08-18 14:02:02 +0900

    챕터3 비교연산자(관계연산자)

---
+++ doitjava/src/chapter03/CompareOperator.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class CompareOperator {
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 10;
+		
+		boolean res1 = (num1 == num2);
+		boolean res2 = (num1 != num2);
+		boolean res3 = (num1 <= num2);
+		System.out.println("res1 = " + res1);
+		System.out.println("res2 = " + res2);
+		System.out.println("res3 = " + res3);
+		
+		char char1 = 'A', char2 = 'B';
+		boolean res4 = (char1 < char2);
+		System.out.println("res4 = " + res4);
+	}
+}
