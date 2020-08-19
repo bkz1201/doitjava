d2658fdcfcd65c00185bdf9994ac1027ff3aad28
seokjin <Yseokjin.dev@gmail.com>
2020-08-19 14:11:34 +0900

    챕터4 if문 예제01

--- doitjava/src/chapter04/IfEx01.java
+++ doitjava/src/chapter04/IfEx01.java
@@ -2,17 +2,20 @@ package chapter04;
 
 public class IfEx01 {
 	public static void main(String[] args) {
-		int score = 93;
+		int score = 80;
 
 		if (score >= 90) {
 			System.out.println("점수가 90보다 크거나 같습니다.");
 			System.out.println("등급은 A입니다.");
-		}
-
-		if (score < 90) {
+		} else {	//else는 if문의 조건이 false일때 실행됩니다.
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B입니다.");
 		}
+
+//		if (score < 90) {
+//			System.out.println("점수가 90보다 작습니다.");
+//			System.out.println("등급은 B입니다.");
+//		}
 
 		System.out.println("The End");
 	}
