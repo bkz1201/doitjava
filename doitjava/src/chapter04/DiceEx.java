e8b08885d0b0e13dcbf7503e184d912d3bbd42a8
seokjin <Yseokjin.dev@gmail.com>
2020-08-20 15:01:43 +0900

    다이스 예제 1

---
+++ doitjava/src/chapter04/DiceEx.java
@@ -0,0 +1,30 @@
+package chapter04;
+
+public class DiceEx {
+	public static void main(String[] args) {
+		int dice = (int) (Math.random() * 6) + 1;
+
+		System.out.println(dice);
+
+		if (dice <= 3) {
+			if (dice == 1)
+				System.out.println("주사위의 눈금은 1입니다.");
+			else if (dice == 2)
+				System.out.println("주사위의 눈금은 2입니다.");
+			else
+				System.out.println("주사위의 눈금은 3입니다.");
+		} else {
+			if (dice == 4)
+				System.out.println("주사위의 눈금은 4입니다.");
+			else if (dice == 5)
+				System.out.println("주사위의 눈금은 5입니다.");
+			else
+				System.out.println("주사위의 눈금은 6입니다.");
+		}
+	}
+}
+
+/*
+ * Math.random() 은 0이상 1미만의 수 0 <= Math.random() < 1 0*6+1 <= Math.random()*6+1
+ * < 1*6+1
+ */
