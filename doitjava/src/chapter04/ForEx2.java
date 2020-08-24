ee0823403486b4832dd763765356c320ba13652b
seokjin <Yseokjin.dev@gmail.com>
2020-08-24 14:18:59 +0900

    챕터4 for ex2

---
+++ doitjava/src/chapter04/ForEx2.java
@@ -0,0 +1,12 @@
+package chapter04;
+
+public class ForEx2 {
+	public static void main(String[] args) {
+//		int i = 0;
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("안녕하세요! "+(i+1)+"트");
+		}
+//		System.out.println(i);
+	}
+}
