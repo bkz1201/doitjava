01f488093633784f7fa5105844afda676805b3fc
seokjin <Yseokjin.dev@gmail.com>
2020-08-24 13:45:31 +0900

    챕터4 dowhile ex2

---
+++ doitjava/src/chapter04/DoWhileEx2.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class DoWhileEx2 {
+	public static void main(String[] args) {
+		System.out.println("메시지를 입력하세요.");
+		System.out.println("프로그램을 종료하려면 q를 입력하세요.");
+
+		Scanner scan = new Scanner(System.in);
+		String iStr;
+
+		do {
+			System.out.print("> ");
+			iStr = scan.nextLine();
+			System.out.println(iStr);
+		} while (!iStr.equals("q"));
+		System.out.println();
+		System.out.println("프로그램 종료");
+		scan.close();
+	}
+}
