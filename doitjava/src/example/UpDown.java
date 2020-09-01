9a37b67164298ccb83b3a34550bc47d3f1e50b9f
seokjin <Yseokjin.dev@gmail.com>
2020-09-01 14:08:23 +0900

    업다운

--- doitjava/src/example/UpDown.java
+++ doitjava/src/example/UpDown.java
@@ -9,27 +9,31 @@ public class UpDown {
 		Scanner sc = new Scanner(System.in);
 
 		int sN = 1, lN = 100;
-		//System.out.println(rand);
+		// System.out.println(rand);
 		for (;;) {
 			System.out.print("생성된 숫자는 무엇인가요?");
-			System.out.println("(범위 : " + sN + " ~ " + lN+")");
+			System.out.println("(범위 : " + sN + " ~ " + lN + ")");
 			int i = sc.nextInt();
 			cnt++;
-			if (i > rand) {
+			System.out.println(cnt + "회 입력");
+			if (i < sN || i > lN) {
+				System.out.println("잘못 입력하셨습니다.");
+
+				continue;
+			} else if (i > rand) {
 				System.out.print("다운");
-				System.out.println(cnt+"회 입력");
 				lN = i - 1;
 			} else if (i < rand) {
 				System.out.println("업");
-				System.out.println(cnt+"회 입력");
 				sN = i + 1;
 			} else {
+				System.out.println();
 				System.out.println("정답!");
 				break;
 			}
 			System.out.println();
 		}
-		System.out.println("정답을 찾는데 걸린 횟수 : " + cnt +"회");
+		System.out.println("정답을 찾는데 걸린 횟수 : " + cnt + "회");
 		sc.close();
 	}
 }
