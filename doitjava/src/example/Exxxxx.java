521036c06ced03366b4b608db4fc02e64442ddfe
seokjin <Yseokjin.dev@gmail.com>
2020-09-02 15:55:16 +0900

    복습

---
+++ doitjava/src/example/Exxxxx.java
@@ -0,0 +1,23 @@
+package example;
+
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
+public class Exxxxx {
+	public static void main(String[] args) {
+//		char a='A';
+//		
+//		System.out.println((int)a);	//char 숫자대입가능
+//
+//		byte bNum = Byte.MAX_VALUE;
+//		System.out.println(bNum);
+//		System.out.println(++bNum);
+//		System.out.println(++bNum);
+//		int iNum = Integer.MIN_VALUE;
+//		System.out.println(iNum);
+//		System.out.println(--iNum);
+//		System.out.println(--iNum);	//오버플로우
+		
+		
+		//값으 내용 비교시 equals
+	}
+}
