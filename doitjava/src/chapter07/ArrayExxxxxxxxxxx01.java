5287c1c7cf39590bd10e5172c7f5618d04a4285f
Yoon Seok Jin <yseokjin.dev@gmail.com>
2020-09-04 16:07:25 +0900

    아무튼 알파벳 정렬!

--- doitjava/src/chapter07/ArrayExxxxxxxxxxx01.java
+++ doitjava/src/chapter07/ArrayExxxxxxxxxxx01.java
@@ -2,6 +2,7 @@ package chapter07;
 
 public class ArrayExxxxxxxxxxx01 {
 	public static void main(String[] args) {
+		//알파벳 소문자를 2글자씩 13줄로 출력하는 프로그램을 이차원 배열로 구현해 보세요. 221p
 		char[][] cArr = new char[13][2];
 		int flag = 97;
 		for (int i = 0; i < cArr.length; i++) {
